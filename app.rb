a34258dc8004193d74bb3b589d7e3e2ecd4a0c79
François Beausoleil <francois@teksol.info>
2008-06-28 00:23:02 -0400

    Initial view of web role data.

---
+++ app.rb
@@ -0,0 +1,19 @@
+require "rubygems"
+require "sinatra"
+require "gchart"
+
+get "/web0" do
+  hits = web0_data["hits_per_domain"]
+  hits.each_pair do |scale, values|
+    top10 = instance_variable_set("@top10_#{scale}", values.sort_by(&:last).reverse[0, 10])
+    instance_variable_set("@hits_per_domain_#{scale}", Gchart.pie(:data => top10.map(&:last), :legend => top10.map(&:first), :size => "360x200"))
+  end
+
+  erb :web0
+end
+
+helpers do
+  def web0_data
+    @web0_data ||= YAML.load_file("web0.yml")
+  end
+end
