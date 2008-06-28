0d44a00e52193ab9a3e5a657f37b2e425b85f239
François Beausoleil <francois@teksol.info>
2008-06-28 09:41:54 -0400

    Renamed web0.erb to hits_per_domain.erb.  Changed URL for hits per domain.

--- app.rb
+++ app.rb
@@ -2,14 +2,14 @@ require "rubygems"
 require "sinatra"
 require "gchart"
 
-get "/web0" do
+get "/hits-per-domain" do
   hits = web0_data["hits_per_domain"]
   hits.each_pair do |scale, values|
     top10 = instance_variable_set("@top10_#{scale}", values.sort_by(&:last).reverse[0, 10])
     instance_variable_set("@hits_per_domain_#{scale}", Gchart.pie(:data => top10.map(&:last), :legend => top10.map(&:first), :size => "360x200"))
   end
 
-  erb :web0
+  erb :hits_per_domain
 end
 
 helpers do
