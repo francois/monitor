22f3a882adebdb1600c4413357a50bc419215476
François Beausoleil <francois@teksol.info>
2008-07-01 00:11:07 -0400

    Proper bar chart with colors for disk free space.
    New #number_to_human_size helper (copied from ActionPack).

--- app.rb
+++ app.rb
@@ -1,12 +1,15 @@
 require "rubygems"
 require "sinatra"
 require "gchart"
+require "activesupport"
 
 get "/hits-per-domain" do
-  hits = web0_data["hits_per_domain"]
-  hits.each_pair do |scale, values|
-    top10 = instance_variable_set("@top10_#{scale}", values.sort_by(&:last).reverse[0, 10])
-    instance_variable_set("@hits_per_domain_#{scale}", Gchart.pie(:data => top10.map(&:last), :legend => top10.map(&:first), :size => "360x200"))
+  web_data.each do |web|
+    hits = web["hits_per_domain"]
+    hits.each_pair do |scale, values|
+      top10 = instance_variable_set("@top10_#{scale}", values.sort_by(&:last).reverse[0, 10])
+      instance_variable_set("@hits_per_domain_#{scale}", Gchart.pie(:data => top10.map(&:last), :legend => top10.map(&:first), :size => "360x200"))
+    end
   end
 
   erb :hits_per_domain
@@ -14,16 +17,49 @@ end
 
 get "/disk-free" do
   @disk = []
-  disk = web0_data["disk"]
-  disk.each_pair do |disk, data|
-    @disk << ["web0@#{data["mountpoint"]}", data["free"]]
+  all_data.each do |data|
+    name = data["name"]
+    disk = data["disk"]
+    disk.each_pair do |disk, data|
+      @disk << ["#{name}@#{data["mountpoint"]}", data["free"]]
+    end
   end
 
+  @disk.sort!
   erb :disk_free
 end
 
 helpers do
-  def web0_data
-    @web0_data ||= YAML.load_file("web0.yml")
+  def all_data
+    return @result if @result
+    @result = Array.new
+    Dir["data/*.yml"].each do |file|
+      @result << YAML.load_file(file)
+    end
+    @result
+  end
+
+  def web_data
+    return @result if @result
+    @result = Array.new
+    Dir["data/web*.yml"].each do |file|
+      @result << YAML.load_file(file)
+    end
+    @result
+  end
+
+  # Copied from ActionPack 2.1.0.
+  def number_to_human_size(size, precision=1)
+    size = Kernel.Float(size)
+    case
+      when size.to_i == 1;    "1 Byte"
+      when size < 1.kilobyte; "%d Bytes" % size
+      when size < 1.megabyte; "%.#{precision}f KB"  % (size / 1.0.kilobyte)
+      when size < 1.gigabyte; "%.#{precision}f MB"  % (size / 1.0.megabyte)
+      when size < 1.terabyte; "%.#{precision}f GB"  % (size / 1.0.gigabyte)
+      else                    "%.#{precision}f TB"  % (size / 1.0.terabyte)
+    end.sub(/([0-9]\.\d*?)0+ /, '\1 ' ).sub(/\. /,' ')
+  rescue
+    nil
   end
 end
